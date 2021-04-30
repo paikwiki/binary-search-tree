d49ab0a158d2d33a7e47f21671d69a5b278b2efd
paikwiki <paikwiki@gmail.com>
2021-04-30 22:38:31 +0900

    Edit BSTree(WIP, not working)

--- code/include/BSTree.hpp
+++ code/include/BSTree.hpp
@@ -6,18 +6,27 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 19:49:51 by cbaek             #+#    #+#             */
-/*   Updated: 2021/04/30 21:33:02 by cbaek            ###   ########.fr       */
+/*   Updated: 2021/04/30 22:38:02 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef B_S_TREE_H
 #define B_S_TREE_H
 
-template <typename T>
+#include <iostream>
+
+#include "../include/Node.hpp"
+template <typename _Node>
 class BSTree {
  private:
- public:
+  _Node *root;
 
+  BSTree();
+ public:
+  BSTree(BSTree cosnt &bstree);
+  ~BSTree();
+
+  BSTree &operator=(BSTree const &rhs) {};
 };
 
 #endif
--- code/tests/BSTree_test.cpp
+++ code/tests/BSTree_test.cpp
@@ -6,7 +6,21 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 20:01:10 by cbaek             #+#    #+#             */
-/*   Updated: 2021/04/30 20:01:11 by cbaek            ###   ########.fr       */
+/*   Updated: 2021/04/30 22:33:07 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../include/BSTree.hpp"
+#include "gtest/gtest.h"
+
+TEST(BSTree는, 생성시_반드시_노드를_갖는다) {
+  // setup
+  Node<int> *node = new Node<int>(42);
+  BSTree< Node<int> > *bst = new BSTree< Node<int> >(node);
+
+  // test
+  EXPECT_EQ(bst->getRoot(), *node);
+
+  // teardown
+  delete node;
+}
