2956904d6b61e0b3be11699566117a82ca6744a7
paikwiki <paikwiki@gmail.com>
2021-05-01 15:38:12 +0900

    Remove comment

--- code/include/Node.hpp
+++ code/include/Node.hpp
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 21:32:47 by cbaek             #+#    #+#             */
-/*   Updated: 2021/05/01 15:20:14 by cbaek            ###   ########.fr       */
+/*   Updated: 2021/05/01 15:37:58 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,6 @@
 template <typename T>
 class Node {
  private:
-  // T key;
-  // Node *left;
-  // Node *right;
-  // Node *parent;
-
   Node() {}
 
  public:
@@ -41,28 +36,6 @@ class Node {
   ~Node(){};
 
   Node &operator=(Node const &rhs) {}
-
-  // void setLeft(Node &node) { this->left = &node; }
-  // void setRight(Node &node) { this->right = &node; }
-  // void setParent(Node &node) { this->parent = &node; }
-
-  // Node<T> const &getLeft() const {
-  //   if (this->left == static_cast<Node<T> *>(0))
-  //     return (static_cast<Node<T> >(0));  // TODO: warning 제거
-  //   return (*(this->left));
-  // }
-  // Node<T> const &getRight() const {
-  //   if (this->right == static_cast<Node<T> *>(0))
-  //     return (static_cast<Node<T> >(0));  // TODO: warning 제거
-  //   return (*(this->right));
-  // }
-  // Node<T> const &getParent() const {
-  //   if (this->parent == static_cast<Node<T> *>(0))
-  //     return (static_cast<Node<T> >(0));  // TODO: warning 제거
-  //   return (*(this->parent));
-  // }
-
-  // T const &getKey() const { return (this->key); }
 
   friend bool operator==(Node const &lhs, Node const &rhs) {
     return (lhs.key == rhs.key);
